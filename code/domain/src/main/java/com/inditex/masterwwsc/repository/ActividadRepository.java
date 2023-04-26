e5874fb32ec329faf81331857d572e805c8ce825
Pablo Albacete Rodriguez <pabloalbrdz02@gmail.com>
2023-04-26 17:24:56 +0200

    Added entity, repository and controller from the actividad table

--- code/domain/src/main/java/com/inditex/masterwwsc/repository/ActividadRepository.java
+++ code/domain/src/main/java/com/inditex/masterwwsc/repository/ActividadRepository.java
@@ -1,2 +1,9 @@
-package com.inditex.masterwwsc.repository;public interface ActividadRepository {
+package com.inditex.masterwwsc.repository;
+
+import com.inditex.masterwwsc.entities.Actividad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigInteger;
+
+public interface ActividadRepository extends JpaRepository<Actividad, BigInteger> {
 }
--- code/infrastructure/src/main/java/com/inditex/masterwwsc/ActividadController.java
+++ code/infrastructure/src/main/java/com/inditex/masterwwsc/ActividadController.java
@@ -1,2 +1,23 @@
-package com.inditex.masterwwsc;public class ActividadController {
+package com.inditex.masterwwsc;
+
+import com.inditex.masterwwsc.entities.Actividad;
+import com.inditex.masterwwsc.repository.ActividadRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class ActividadController {
+
+    private ActividadRepository actividadRepository;
+
+    public ActividadController(ActividadRepository actividadRepository) {
+        this.actividadRepository = actividadRepository;
+    }
+
+    @GetMapping(value = "/actividad", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Actividad>> getAllActivities(){
+        return ResponseEntity.ok(actividadRepository.findAll());
+    }
 }
