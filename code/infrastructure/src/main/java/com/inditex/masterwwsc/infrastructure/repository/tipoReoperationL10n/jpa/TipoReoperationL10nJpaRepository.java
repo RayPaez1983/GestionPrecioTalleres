47a5849255bb4aa1935b4c774a42a7312c346679
Daniel <dagonbu02@gmail.com>
2023-05-04 16:07:49 +0200

    Changes by p-rgarciaredondo

--- code/infrastructure/src/main/java/com/inditex/masterwwsc/infrastructure/repository/tipoReoperationL10n/jpa/TipoReoperationL10nJpaRepository.java
+++ code/infrastructure/src/main/java/com/inditex/masterwwsc/infrastructure/repository/tipoReoperationL10n/jpa/TipoReoperationL10nJpaRepository.java
@@ -1,11 +1,12 @@
 package com.inditex.masterwwsc.infrastructure.repository.tipoReoperationL10n.jpa;
 
-import com.inditex.masterwwsc.infrastructure.domain.entity.InMemoryTipoReoperacionI10nEntity;
+import com.inditex.masterwwsc.infrastructure.domain.entity.InMemoryTipoReoperacionL10nEntity;
+import com.inditex.masterwwsc.infrastructure.domain.entity.TipoReoperacionL10nEmbeddable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 
 @Repository
-public interface TipoReoperationL10nJpaRepository extends JpaRepository<InMemoryTipoReoperacionI10nEntity, BigInteger> {
+public interface TipoReoperationL10nJpaRepository extends JpaRepository<InMemoryTipoReoperacionL10nEntity, TipoReoperacionL10nEmbeddable> {
 }
